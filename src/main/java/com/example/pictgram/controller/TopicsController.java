64852f6d85de557eba48130e14188580b3015339
kamo-eri <ekamo.tech@gmail.com>
2024-11-18 17:19:28 +0900

    お気に入り画面の作成

--- src/main/java/com/example/pictgram/controller/TopicsController.java
+++ src/main/java/com/example/pictgram/controller/TopicsController.java
@@ -31,8 +31,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.example.pictgram.entity.Favorite;
 import com.example.pictgram.entity.Topic;
 import com.example.pictgram.entity.UserInf;
+import com.example.pictgram.form.FavoriteForm;
 import com.example.pictgram.form.TopicForm;
 import com.example.pictgram.form.UserForm;
 import com.example.pictgram.repository.TopicRepository;
@@ -72,6 +74,12 @@ public class TopicsController {
     public TopicForm getTopic(UserInf user, Topic entity) throws FileNotFoundException, IOException {
         modelMapper.getConfiguration().setAmbiguityIgnored(true);
         modelMapper.typeMap(Topic.class, TopicForm.class).addMappings(mapper -> mapper.skip(TopicForm::setUser));
+        
+        modelMapper.typeMap(Topic.class, TopicForm.class).addMappings(mapper -> mapper.skip(TopicForm::setFavorites));
+        modelMapper.typeMap(Favorite.class, FavoriteForm.class).addMappings(mapper -> mapper.skip(FavoriteForm::setTopic));
+        
+        
+        
 
         boolean isImageLocal = false;
         if (imageLocal != null) {
@@ -100,6 +108,18 @@ public class TopicsController {
 
         UserForm userForm = modelMapper.map(entity.getUser(), UserForm.class);
         form.setUser(userForm);
+        
+        List<FavoriteForm> favorites = new ArrayList<FavoriteForm>();
+        for (Favorite favoriteEntity : entity.getFavorites()) {
+            FavoriteForm favorite = modelMapper.map(favoriteEntity, FavoriteForm.class);
+            favorites.add(favorite);
+            if (user.getUserId().equals(favoriteEntity.getUserId())) {
+                form.setFavorite(favorite);
+            }
+        }
+        form.setFavorites(favorites);
+        
+        
 
         return form;
     }
